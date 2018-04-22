ce25fdccfb74d40f828c3a61e7cab6a358ce6e5c
maxtheaxe <max.perrello@gmail.com>
2018-04-22 01:39:40 -0400

    Update button.py

--- button.py
+++ button.py
@@ -4,7 +4,6 @@ import RPi.GPIO as GPIO
 
 url = 'http://nightlight/' # I map this to my internal DNS hosting the node app
 gpio_pin=18 # The GPIO pin the button is attached to
-longpress_threshold=5 # If button is held this length of time, tells system to leave light on
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(gpio_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
@@ -14,16 +13,4 @@ while True:
 
     if GPIO.input(gpio_pin) == False: # Listen for the press, the loop until it steps
         print "Started press"
-        pressed_time=time.time()
-        while GPIO.input(gpio_pin) == False:
-            time.sleep(0.2)
-
-        pressed_time=time.time()-pressed_time
-        print "Button pressed %d, POSTing to nightlight server" % pressed_time
-        if pressed_time<longpress_threshold:
-            data = dict(light=2, cmd="toggle-with-timer")
-        else:
-            data = dict(light=2, cmd="toggle-keep-on")
-
-        r = requests.post(url, data=data, allow_redirects=True)
-        print r.content
+        
